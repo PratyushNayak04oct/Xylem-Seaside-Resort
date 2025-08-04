9fd7e53495cb6cfbdeb7293caf97f35903a53ad4
Pratyush <pratyush041002@gmail.com>
2025-08-04 21:56:04 +0530

    Adds various pages and routes

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      This is the About US page.
+    </div>
+  )
+}
+
+export default About
---
+++ app/amenities/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Amenities = () => {
+  return (
+    <div>
+      This is the amenities page.
+    </div>
+  )
+}
+
+export default Amenities ; 
