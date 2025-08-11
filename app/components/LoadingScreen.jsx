c07512e0a8b8f5ad19d7c22c6778e06f0d6e28e4
Pratyush <pratyush041002@gmail.com>
2025-08-11 11:12:07 +0530

    Makes the Animated logo center and responsive

--- app/components/LoadingScreen.jsx
+++ app/components/LoadingScreen.jsx
@@ -3,11 +3,11 @@
 import React from "react";
 import Image from "next/image";
 import AnimatedLogo from "./AnimatedLogo";
-import bgImage from "../../public/Loading-Screen-Image.webp" ; 
+import bgImage from "../../public/Loading-Screen-Image.webp";
 
 const LoadingScreen = () => {
   return (
-    <div className="relative h-screen w-screen flex items-center justify-center">
+    <div className="relative h-screen w-screen overflow-hidden">
       <Image
         src={bgImage}
         alt="Loading Screen"
@@ -16,8 +16,11 @@ const LoadingScreen = () => {
         quality={80}
         placeholder="blur"
         className="object-cover"
+        sizes="100vw"
       />
-      <AnimatedLogo className="z-10" />
+      <div className="absolute inset-0 flex items-center justify-center p-4">
+        <AnimatedLogo />
+      </div>
     </div>
   );
 };
